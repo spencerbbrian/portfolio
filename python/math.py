6cc469542b6aeaef8123058fce4f6cf00c275f24
Spencer Brian <brianbaiden@gmail.com>
2024-02-09 13:44:24 +0000

    math.py complete

--- python/math.py
+++ python/math.py
@@ -1,8 +1,10 @@
 import random
+import time
 
 OPERATORS = ["+", "-", "*"]
 MIN_OPERAND = 3
 MAX_OPERAND = 12
+TOTAL_PROBLEMS = 3
 
 def generate_problem():
     left = random.randint(MIN_OPERAND,MAX_OPERAND)
@@ -10,6 +12,25 @@ def generate_problem():
     operator = random.choice(OPERATORS)
 
     expression = str(left) + " " + operator + " " + str(right)
-    print(expression)
+    answer = eval(expression)
+    return expression, answer
 
-generate_problem
+wrong = 0
+input("Press enter to start!")
+print("--------------------")
+
+start_time = time.time()
+
+for i in range(TOTAL_PROBLEMS):
+    expression, answer = generate_problem()
+    while True:
+        guess = input("Problem #" + str(i + 1) + ": " + expression + " = ")
+        if guess == str(answer):
+            break
+        wrong += 1
+
+end_time = time.time()
+total_time = round(end_time - start_time,2)
+
+print("----------------------")
+print("Nice work! You finished in", total_time, "seconds!")

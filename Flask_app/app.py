71170bfe5bb89471062eadb73ecb0542e0e6fd11
Spencer Brian <brianbaiden@gmail.com>
2024-04-03 22:16:17 +0000

    flask app start

--- Flask_app/app.py
+++ Flask_app/app.py
@@ -1,3 +1,15 @@
-from flask import Flask
+from flask import Flask,render_template
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
+
+#My App
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ in "__main__":
+    app.run(debug=True)

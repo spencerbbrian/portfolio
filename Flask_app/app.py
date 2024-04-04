5f6356ec9007efbf5a1120c1edc542e0361614a1
Spencer Brian <brianbaiden@gmail.com>
2024-04-04 22:50:10 +0000

    app complete

--- Flask_app/app.py
+++ Flask_app/app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 Scss(app)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATION"] = "False"
 db = SQLAlchemy(app)
 
 #Data Class ~ Row of data
@@ -19,6 +20,9 @@ class MyTask(db.Model):
 
     def __repr__(self) -> str:
         return f"Task {self.id}"
+    
+with app.app_context():
+        db.create_all()
 
 #Home Page
 @app.route("/",methods=["POST","GET"])
@@ -66,7 +70,5 @@ def edit(id:int):
         return render_template('edit.html',task=task)
 
 
-if __name__ in "__main__":
-    with app.app_context():
-        db.create_all()
+if __name__ == "__main__":
     app.run(debug=True)

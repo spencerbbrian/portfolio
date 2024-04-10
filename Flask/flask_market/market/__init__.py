34919ef5baa8a5948bac5990180d6e0811598781
Spencer Brian <brianbaiden@gmail.com>
2024-04-10 15:52:51 +0000

    style flask form

--- Flask/flask_market/market/__init__.py
+++ Flask/flask_market/market/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATION"] = "False"
+app.config['SECRET_KEY'] = 'b2fefc360330792a78552f7c'
 db = SQLAlchemy(app)
 
 # with app.app_context():

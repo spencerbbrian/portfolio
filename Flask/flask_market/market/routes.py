db847a5bf47bb72b3b8a48227f014050d6238184
Spencer Brian <brianbaiden@gmail.com>
2024-04-12 22:16:09 +0000

    complete alerts for flash messages

--- Flask/flask_market/market/routes.py
+++ Flask/flask_market/market/routes.py
@@ -46,5 +46,5 @@ def register_page():
         return redirect(url_for('market_page'))
     if form.errors != {}: #If there are errors from the validations in the model
         for err_msg in form.errors.values():
-            flash(f'There was an error with creating a user: {err_msg}')
+            flash(f'There was an error with creating a user: {err_msg}', category='danger')
     return render_template('register.html', form=form)

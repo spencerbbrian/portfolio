991ade1d26569c8d7e542f9d682d668282ecaa79
Spencer Brian <brianbaiden@gmail.com>
2024-04-17 16:23:34 +0000

    create hashed passwords

--- Flask/flask_market/market/__init__.py
+++ Flask/flask_market/market/__init__.py
@@ -1,13 +1,16 @@
-from flask import Flask,render_template,request, redirect
+from flask import Flask,request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATION"] = "False"
 app.config['SECRET_KEY'] = 'b2fefc360330792a78552f7c'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+from market import routes
 
 # with app.app_context():
 #         db.create_all()
 
-from market import routes

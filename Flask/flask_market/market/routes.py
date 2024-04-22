16ca7148df555355bb141b6f0595a42817cd7e1f
Spencer Brian <brianbaiden@gmail.com>
2024-04-22 02:03:46 +0000

    item selling function complete

--- Flask/flask_market/market/routes.py
+++ Flask/flask_market/market/routes.py
@@ -34,8 +34,10 @@ def add_page():
 @login_required
 def market_page():
     purchase_form = PurchaseItemForm()
+    selling_form = SellItemForm()
     if request.method == "POST":
-        purchased_item = request.form.get('purchased_item')
+        #Purchase Item Logic
+        purchased_item = request.form.get('purchased_item') #This is the name in the input of the modal which gets the value of that input
         p_item_object = Item.query.filter_by(name=purchased_item).first()
         if p_item_object:
             if current_user.can_purchase(p_item_object):
@@ -43,11 +45,22 @@ def market_page():
                 flash(f"Congratulations! You purchased {p_item_object.name} for ${p_item_object.price}",category='success')
             else:
                 flash(f"Unfortunately, you don't have enough to purchase {p_item_object.name}!",category='danger')
+        #Sell Item Logic
+        sold_item = request.form.get('sold_item')
+        s_item_object = Item.query.filter_by(name=sold_item).first()
+        if s_item_object:
+            #ensure ownership of item
+            if current_user.can_sell(s_item_object):
+                s_item_object.sell(current_user)
+                flash(f"Congratulations! You sold {s_item_object.name} for ${s_item_object.price}",category='success')
+            else:
+                flash(f"Unfortunately, you couldn't sell {s_item_object.name}!",category='danger')
         return redirect(url_for('market_page'))
     
     if request.method == "GET":
         items = Item.query.filter_by(owner=None)
-        return render_template('market.html',items=items,purchase_form=purchase_form)
+        owned_items = Item.query.filter_by(owner=current_user.id)
+        return render_template('market.html',items=items,purchase_form=purchase_form,owned_items=owned_items,selling_form=selling_form)
 
 @app.route('/register', methods=["GET", "POST"])
 def register_page():

e419802b6da7c992c2ddc85944f6404f8487b2b9
Spencer Brian <brianbaiden@gmail.com>
2024-04-30 05:46:21 +0000

    forms update

--- data-engineering-projects/real-time-stock-price-analysis/stocks/routes.py
+++ data-engineering-projects/real-time-stock-price-analysis/stocks/routes.py
@@ -1,10 +1,18 @@
 from stocks import app, db
 from stocks.models import Stock
+from stocks.forms import SimulateForm
 from flask import Flask, render_template, request, redirect, url_for, flash
 
-@app.route('/')
-@app.route('/home', methods=['GET'])
+@app.route('/', methods=['GET','POST'])
 def home_page():
+    simulate_form = SimulateForm()
+     #Simulate Logic
+    if request.method == 'POST':
+        stock_to_simulate = request.form.get('simulate_stock')
+        stocks_check = Stock.query.filter_by(stock=stock_to_simulate).first()
+        if stocks_check:
+            print('stocks_check')
+
     if request.method == 'GET':
         stock_items = Stock.query.filter_by(week=1)
-        return render_template('home.html',stock_items=stock_items)   
+    return render_template('home.html',stock_items=stock_items,simulate_form=simulate_form)

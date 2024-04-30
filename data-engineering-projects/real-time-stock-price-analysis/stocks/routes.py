b414405a4f771a7f7a0f405bbb33e7daa199df2c
Spencer Brian <brianbaiden@gmail.com>
2024-04-30 04:39:35 +0000

    info modal complete

--- data-engineering-projects/real-time-stock-price-analysis/stocks/routes.py
+++ data-engineering-projects/real-time-stock-price-analysis/stocks/routes.py
@@ -2,10 +2,9 @@ from stocks import app, db
 from stocks.models import Stock
 from flask import Flask, render_template, request, redirect, url_for, flash
 
-
+@app.route('/')
 @app.route('/home', methods=['GET'])
 def home_page():
     if request.method == 'GET':
-        stock_items = Stock.query.all()
-        return render_template('home.html',stock_items=stock_items)
-    
+        stock_items = Stock.query.filter_by(week=1)
+        return render_template('home.html',stock_items=stock_items)   

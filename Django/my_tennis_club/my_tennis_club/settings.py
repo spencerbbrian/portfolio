151ec364adb71aa34e6c9f3b4a331bdc43a1280a
Spencer Brian <brianbaiden@gmail.com>
2024-10-20 02:08:21 +0200

    Refactor project structure and add new views for member details and main page

--- Django/my_tennis_club/my_tennis_club/settings.py
+++ Django/my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b@@au%0&k6^o64n0@_$tdm3qpa7u=zfwwjtd0op1%bp0vyjv)v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

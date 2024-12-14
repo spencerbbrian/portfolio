f3524687d5efc75ea3ce8c596ea251ae0ee09691
Spencer Brian <brianbaiden@gmail.com>
2024-12-14 18:02:27 +0100

    Refactor README notebook to streamline currency pair selection and exchange rate retrieval; remove unused code and improve error handling

--- data-engineering-projects/banking-system/src/transaction_simulator.py
+++ data-engineering-projects/banking-system/src/transaction_simulator.py
@@ -31,32 +31,29 @@ def get_next_transaction_id(db):
         
     return new_id
 
+
 def generate_transactions(db):
     try:
         transaction_type = random.choice(TRANSACTION_TYPES)
-        print(f"Selected transaction type: {transaction_type}")
         deposit_withdrawal_amount = round(random.uniform(100.0, 1000.0), 2)
-        if deposit_withdrawal_amount <= 0:
-            print("Invalid transaction amount.")
-            return
+
         primary_account = random.choice(list(db.accounts.find({"account_category": "Customer"})))
+        primary_account_currency = primary_account["currency"]
+        primary_minimum_balance = primary_account["minimum_balance"]
+        primary_account_balance = primary_account["current_balance"]
+
         secondary_account = random.choice(list(db.accounts.find({
         "account_category": "Customer", 
         "_id": {"$ne": primary_account["_id"]}
         })))
-    
-
-        bank_charge = round(random.uniform(0.5, 1.0), 2)
-
-        minimum_balance = primary_account["minimum_balance"]
-        primary_account_balance = primary_account["current_balance"]
-        secondary_account_balance = secondary_account["current_balance"]
-
-        primary_account_currency = primary_account["currency"]
         secondary_account_currency = secondary_account["currency"]
+        secondary_minimum_balance = primary_account["minimum_balance"]
+        secondary_account_balance = secondary_account["current_balance"]
+    
+        bank_charge = round(random.uniform(0.5, 1.0), 2)
     
         if transaction_type == "deposit":
-            new_balance = primary_account["current_balance"] + deposit_withdrawal_amount
+            new_balance = primary_account_balance + deposit_withdrawal_amount
             print(f"Depositing {deposit_withdrawal_amount} into account {primary_account['account_id']}.")
             db.accounts.update_one(
                 {"_id": primary_account['_id']},
@@ -80,9 +77,10 @@ def generate_transactions(db):
             total_withdrawal_amount = deposit_withdrawal_amount + bank_charge
             temp_balance = primary_account_balance - total_withdrawal_amount
             if total_withdrawal_amount < primary_account_balance:
-                if temp_balance > minimum_balance:
+                if temp_balance > primary_minimum_balance:
                     print(f"Withdrawing {deposit_withdrawal_amount} from account {primary_account['account_id']}.")
                     db.accounts.update_one({"_id": primary_account['_id']},{"$set": {"current_balance": temp_balance}})
+
                     transaction = {
                     "transaction_id": get_next_transaction_id(db),
                     "transaction_type": transaction_type,
@@ -91,9 +89,10 @@ def generate_transactions(db):
                     "currency": primary_account["currency"],
                     "sender_account_id": "NA",
                     "receiver_account_id": primary_account["account_id"],
-                    "timestamp": datetime.combine(fake.date_this_month(), datetime.min.time()),
+                    "timestamp": datetime.combine(fake.date_this_month(), datetime.min.time()), 
                     "status": "success"
                     }
+
                     db.transactions.insert_one(transaction)
                     return
                 else:
@@ -106,25 +105,31 @@ def generate_transactions(db):
         elif transaction_type == "purchase" or transaction_type == "POS" or transaction_type == "payment":
             category = random.choice(list(POS_PAYMENTS.keys()))
             random_pos_item = random.choice(POS_PAYMENTS[category])
+
             merchant = random.choice(list(db.merchants.find({"merchant_type": category})))
+
             amount = round(random.uniform(1.0, 1000.0), 2)
-            initial_amount = amount
+        
 
-            if merchant['currency'] != primary_account['currency']:
-                amount = convert_currency(db,f"{primary_account_currency}/{merchant['currency']}",amount)
+            if merchant['currency'] != primary_account_currency:
+                final_amount = convert_currency(db,f"{merchant['currency']}/{primary_account_currency}",amount)
+            else:
+                final_amount = amount
 
-            temp_balance = primary_account["current_balance"] - amount - bank_charge
-            if temp_balance > primary_account["minimum_balance"]:
-                print(f"Making a purchase of {amount} for {random_pos_item} at a(n) {category} merchant.")
+            temp_balance = primary_account["current_balance"] - final_amount - bank_charge
+
+            if temp_balance > primary_minimum_balance:
+                print(f"Making a purchase of {amount} {merchant['currency']} for {random_pos_item} at {category} merchant.")
                 db.accounts.update_one({"_id": primary_account['_id']},{"$set": {"current_balance": temp_balance}})
                 db.accounts.update_one({"_id": merchant['_id']},{"$inc": {"current_balance": amount}})
+
                 transaction = {
                     "transaction_id": get_next_transaction_id(db),
                     "transaction_type": transaction_type,
-                    "intitial_amount": initial_amount,
-                    "amount": amount,
+                    "intitial_amount": amount,
+                    "amount": final_amount,
                     "charge": bank_charge,
-                    "currency": primary_account["currency"],
+                    "currency": merchant["currency"],
                     "sender_account_id": primary_account['account_id'],
                     "receiver_account_id": merchant["account_id"],
                     "timestamp": datetime.combine(fake.date_this_month(), datetime.min.time()),
@@ -137,65 +142,76 @@ def generate_transactions(db):
                 return
             
         elif transaction_type == "cheque deposit":
-            cheque_amount = round(random.uniform(1001.0, 10000.0), 2)
-            initial_amount = cheque_amount
-            if secondary_account_balance > cheque_amount:
+            amount = round(random.uniform(1001.0, 10000.0), 2)
+            temp_balance = secondary_account["current_balance"] - amount - bank_charge
 
-                if primary_account['currency'] != secondary_account['currency']:
-                    cheque_amount = convert_currency(db,f"{secondary_account_currency}/{primary_account_currency}",amount)
+            if secondary_account_balance > amount:
+                if temp_balance > secondary_minimum_balance:
 
-                print(f"Depositing a cheque of {cheque_amount} into account {primary_account['account_id']} from account {secondary_account['account_id']} .")
-                new_balance = primary_account["current_balance"] + cheque_amount
-                secondary_account_balance -= (cheque_amount + bank_charge)
-                db.accounts.update_one({"_id": primary_account['_id']},{"$set": {"current_balance": new_balance}})
-                db.accounts.update_one({"_id": secondary_account['_id']},{"$set": {"current_balance": secondary_account_balance}})
-                transaction = {
-                    "transaction_id": get_next_transaction_id(db),
-                    "transaction_type": transaction_type,
-                    "intitial_amount": initial_amount,
-                    "amount": cheque_amount,
-                    "charge": bank_charge,
-                    "currency": primary_account["currency"],
-                    "sender_account_id": secondary_account['account_id'],
-                    "receiver_account_id": primary_account["account_id"],
-                    "timestamp": datetime.combine(fake.date_this_month(), datetime.min.time()),
-                    "status": "success"
-                    }
-                db.transactions.insert_one(transaction)
-                return
-            else:
-                print(f"Insufficient funds in account {secondary_account['account_id']} to deposit a cheque of {cheque_amount}.")
-                return
+                    if primary_account['currency'] != secondary_account['currency']:
+                        final_amount = convert_currency(db,f"{secondary_account_currency}/{primary_account_currency}",amount)
+                    else:
+                        final_amount = amount
+
+                    print(f"Depositing a cheque of {amount}{secondary_account_currency} into account {primary_account['account_id']} from account {secondary_account['account_id']}.")
+                    primary_account_balance += final_amount
+                    secondary_account_balance -= (amount + bank_charge)
+
+                    db.accounts.update_one({"_id": primary_account['_id']},{"$set": {"current_balance": primary_account_balance}})
+                    db.accounts.update_one({"_id": secondary_account['_id']},{"$set": {"current_balance": secondary_account_balance}})
+
+                    transaction = {
+                        "transaction_id": get_next_transaction_id(db),
+                        "transaction_type": transaction_type,
+                        "intitial_amount": amount,
+                        "amount": final_amount,
+                        "charge": bank_charge,
+                        "currency": secondary_account_currency,
+                        "sender_account_id": secondary_account['account_id'],
+                        "receiver_account_id": primary_account["account_id"],
+                        "timestamp": datetime.combine(fake.date_this_month(), datetime.min.time()),
+                        "status": "success"
+                        }
+                    db.transactions.insert_one(transaction)
+                    return
+                else:
+                    print(f"Insufficient funds in account {secondary_account['account_id']} to deposit a cheque of {amount}.")
+                    return
             
         elif transaction_type == "cheque withdrawal":
-            cheque_amount = round(random.uniform(1001.0, 10000.0), 2)
-            if secondary_account_balance > cheque_amount:
-                print(f"Withdrawing a cheque of {cheque_amount} from account {secondary_account['account_id']}.")
-                secondary_account_balance -= (cheque_amount + bank_charge)
-                db.accounts.update_one({"_id": secondary_account['_id']},{"$set": {"current_balance": secondary_account_balance}})
-                transaction = {
-                    "transaction_id": get_next_transaction_id(db),
-                    "transaction_type": transaction_type,
-                    "amount": cheque_amount,
-                    "charge": bank_charge,
-                    "currency": secondary_account["currency"],
-                    "sender_account_id": secondary_account['account_id'],
-                    "receiver_account_id": "external",
-                    "timestamp": datetime.combine(fake.date_this_month(), datetime.min.time()),
-                    "status": "success"
-                    }
-                db.transactions.insert_one(transaction)
-                return
+            amount = round(random.uniform(1001.0, 10000.0), 2)
+            temp_balance = secondary_account_balance - amount - bank_charge
+
+            if secondary_account_balance > amount:
+                if temp_balance > secondary_minimum_balance:
+                    print(f"Withdrawing a cheque of {amount}{secondary_account_currency} from account {secondary_account['account_id']}.")
+                    secondary_account_balance -= (amount + bank_charge)
+                    db.accounts.update_one({"_id": secondary_account['_id']},{"$set": {"current_balance": secondary_account_balance}})
+
+                    transaction = {
+                        "transaction_id": get_next_transaction_id(db),
+                        "transaction_type": transaction_type,
+                        "amount": amount,
+                        "charge": bank_charge,
+                        "currency": secondary_account["currency"],
+                        "sender_account_id": secondary_account['account_id'],
+                        "receiver_account_id": "external",
+                        "timestamp": datetime.combine(fake.date_this_month(), datetime.min.time()),
+                        "status": "success"
+                        }
+                    db.transactions.insert_one(transaction)
+                    return
 
         elif transaction_type == "charge":
-            charge_amount = round(random.uniform(1.5, 2.0), 2)
-            new_balance = primary_account["current_balance"] - charge_amount
-            print(f"Charging {charge_amount} to account {primary_account['account_id']}.")
-            db.accounts.update_one({"_id": primary_account['_id']},{"$set": {"current_balance": new_balance}})
+            amount = round(random.uniform(1.5, 2.0), 2)
+            primary_account_balance -= amount
+            print(f"Charging {amount} to account {primary_account['account_id']}.")
+            db.accounts.update_one({"_id": primary_account['_id']},{"$set": {"current_balance": primary_account_balance}})
+
             transaction = {
                     "transaction_id": get_next_transaction_id(db),
                     "transaction_type": transaction_type,
-                    "amount": charge_amount,
+                    "amount": amount,
                     "charge": 0.0,
                     "currency": primary_account["currency"],
                     "sender_account_id": primary_account['account_id'],
@@ -207,23 +223,26 @@ def generate_transactions(db):
             return
         
         elif transaction_type == "transfer":
-            transfer_amount = round(random.uniform(1.0, 1250.0), 2)
-            initial_amount = transfer_amount
-            temp_balance = primary_account_balance - transfer_amount - bank_charge
-            if temp_balance > primary_account["minimum_balance"]:
+            amount = round(random.uniform(1.0, 1250.0), 2)
 
-                if primary_account['currency'] != secondary_account['currency']:
-                    transfer_amount = convert_currency(db,f"{primary_account_currency}/{secondary_account_currency}",amount)
+            temp_balance = primary_account_balance - amount - bank_charge
+            if temp_balance > primary_minimum_balance:
+                if primary_account_currency != secondary_account_currency:
+                    final_amount = convert_currency(db,f"{primary_account_currency}/{secondary_account_currency}",amount)
+                else:
+                    final_amount = amount
 
-                print(f"Transferring {transfer_amount} from account {primary_account['account_id']} to account {secondary_account['account_id']}.")
+                secondary_account_balance += final_amount
+
+                print(f"Transferring {amount}{primary_account_currency} from account {primary_account['account_id']} to account {secondary_account['account_id']}.")
                 db.accounts.update_one({"_id": primary_account['_id']},{"$set": {"current_balance": temp_balance}})
-                secondary_account_balance += transfer_amount
                 db.accounts.update_one({"_id": secondary_account['_id']},{"$set": {"current_balance": secondary_account_balance}})
+
                 transaction = {
                     "transaction_id": get_next_transaction_id(db),
                     "transaction_type": transaction_type,
-                    "intitial_amount": initial_amount,
-                    "amount": transfer_amount,
+                    "intitial_amount": amount,
+                    "amount": final_amount,
                     "charge": bank_charge,
                     "currency": primary_account["currency"],
                     "sender_account_id": primary_account['account_id'],
@@ -234,11 +253,25 @@ def generate_transactions(db):
                 db.transactions.insert_one(transaction)
                 return
             else:
-                print(f"Insufficient funds in account {primary_account['account_id']} to transfer {transfer_amount}.")
+                print(f"Insufficient funds in account {primary_account['account_id']} to transfer {amount}.")
+                log_failed_transaction(db, transaction_type, amount, bank_charge, primary_account_currency, primary_account['account_id'])
                 return               
     except Exception as e:
         print(f"An error occurred: Transaction could not be completed. {e}")
         return
+    
+def log_failed_transaction(db, transaction_type, amount, charge, currency, account_id):
+    transaction = {
+        "transaction_id": get_next_transaction_id(db),
+        "transaction_type": transaction_type,
+        "amount": amount,
+        "charge": charge,
+        "currency": currency,
+        "account_id": account_id,
+        "timestamp": datetime.combine(fake.date_this_month(), datetime.min.time()),
+        "status": "failed"
+    }
+    db.transactions.insert_one(transaction)
     
 def main():
     db = get_db_connection()
